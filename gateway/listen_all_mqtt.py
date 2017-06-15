d82df96d757743fc34ff2db96843981dfaf4dccf
Janne Kujanpää <jikuja@iki.fi>
2017-06-15 17:10:36 +0300

    listen_all_mqtt.py: added websocket support

--- gateway/listen_all_mqtt.py
+++ gateway/listen_all_mqtt.py
@@ -15,11 +15,18 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+
+# Start
 url = urlparse(sys.argv[1])
 if url.scheme == "tcp":
+    transport = "tcp"
+elif url.scheme == "ws":
+    transport = "websockets"
+
+client = mqtt.Client(transport=transport)
+client.on_connect = on_connect
+client.on_message = on_message
+if url.scheme == "tcp" or url.scheme == "ws":
     client.username_pw_set(url.username, url.password)
     client.connect(url.hostname, url.port, 60)
 else:
